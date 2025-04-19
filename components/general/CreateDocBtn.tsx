46ffe6dff53e59097f3993831b649769d1cc70f9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-19 15:05:22 +0545

    create doc btn : skeleton

---
+++ components/general/CreateDocBtn.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../ui/button";
+import { NotebookPen } from "lucide-react";
+const CreateDocBtn = () => {
+  return (
+    <Button>
+      <NotebookPen />
+      Create Doc
+    </Button>
+  );
+};
+
+export default CreateDocBtn;
