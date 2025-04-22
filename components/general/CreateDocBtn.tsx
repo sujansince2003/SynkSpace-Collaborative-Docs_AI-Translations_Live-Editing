669f8c6222de98e1a59bb2d636c4adde6542bfdb
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-22 08:04:29 +0545

    created button to trigger server action to create document in the firebase

--- components/general/CreateDocBtn.tsx
+++ components/general/CreateDocBtn.tsx
@@ -1,8 +1,22 @@
+"use client";
+import { CreateDocument } from "@/actions/action";
 import { Button } from "../ui/button";
 import { NotebookPen } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
 const CreateDocBtn = () => {
+  const router = useRouter();
+  const [ispending, startTransition] = useTransition();
+  function createDoc() {
+    startTransition(async () => {
+      const { docId } = await CreateDocument();
+      router.push(`/docs/${docId}`);
+    });
+  }
+
   return (
-    <Button>
+    <Button disabled={ispending} onClick={createDoc}>
       <NotebookPen />
       Create Doc
     </Button>
