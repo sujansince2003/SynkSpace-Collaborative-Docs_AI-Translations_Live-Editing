12e8e0a1a659aa9dbadfd0a33316032827c2d9d3
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-25 21:56:51 +0545

    main documents section to handle everything related to document

---
+++ components/general/Document.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+import React, { FormEvent, useEffect, useState, useTransition } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { doc, updateDoc } from "firebase/firestore";
+import { firedb } from "@/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import CollabEditor from "./CollabEditor";
+import useOwner from "@/lib/useOwner";
+import DeleteDocument from "./DeleteDocument";
+
+// update title
+//invite other users
+//manage users and avatars
+// collaborative editor
+
+const Document = ({ id }: { id: string }) => {
+  const [input, setInput] = useState("");
+  const [isUpdating, startTransition] = useTransition();
+  const [data, loading, error] = useDocumentData(doc(firedb, "documents", id));
+  const isOwner = useOwner();
+
+  useEffect(() => {
+    if (data) {
+      setInput(data.title);
+    }
+  }, [data]);
+
+  async function updateTitle(e: FormEvent) {
+    e.preventDefault();
+
+    if (input.trim()) {
+      startTransition(async () => {
+        await updateDoc(doc(firedb, "documents", id), {
+          title: input,
+        });
+      });
+    }
+  }
+
+  return (
+    <div className="bg-white flex-1 h-full p-5">
+      <div className="max-w-4xl mx-auto space-x-2 flex justify-between pb-6">
+        <form className="flex flex-1 space-x-2 " onSubmit={updateTitle}>
+          {/* update title and invite users */}
+
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button disabled={isUpdating}>
+            {isUpdating ? "Updating" : "Update"}
+          </Button>
+
+          {/* owner roles */}
+        </form>
+        {isOwner && (
+          <>
+            <DeleteDocument />
+          </>
+        )}
+      </div>
+
+      <div>{/* manaage users and avatars */}</div>
+      <hr className="pb-10" />
+      <CollabEditor />
+      {/* collaborative editing */}
+    </div>
+  );
+};
+
+export default Document;
