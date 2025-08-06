f9c1abad4acbe028af57461f375155ae90f97ce6
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-08-06 23:33:45 +0545

    created the sidebar context for closing and opening collapsable sidebar

---
+++ components/context/SidebarContext.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+type SidebarContextType = {
+  isOpen: boolean;
+  toggle: () => void;
+};
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export const SidebarProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => setIsOpen((prev) => !prev);
+
+  return (
+    <SidebarContext.Provider value={{ isOpen, toggle }}>
+      {children}
+    </SidebarContext.Provider>
+  );
+};
+
+export const useSidebar = () => {
+  const context = useContext(SidebarContext);
+  if (!context)
+    throw new Error("useSidebar must be used within SidebarProvider");
+  return context;
+};
