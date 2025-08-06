cc83003817e00c5ddc7e4079d00818819bd95405
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-08-06 23:30:06 +0545

    added the components from shadcn magicui removed routes files

--- app/doc/layout.tsx
+++
@@ -1,7 +0,0 @@
-import LiveBlocksContainer from "@/components/general/LiveBlocksContainer";
-import React from "react";
-const DocsLayout = ({ children }: { children: React.ReactNode }) => {
-  return <LiveBlocksContainer>{children}</LiveBlocksContainer>;
-};
-
-export default DocsLayout;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-blue-400",
+          "bg-primary text-primary-foreground cursor-pointer shadow-xs hover:bg-primary/90 focus-visible:ring-primary/20 dark:focus-visible:ring-primary/40 dark:bg-primary/60",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
