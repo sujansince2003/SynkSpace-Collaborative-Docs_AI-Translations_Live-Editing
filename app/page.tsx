93ebab87448fb1ce303400cf6f212c4f741b3232
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-08-07 23:44:28 +0545

    updated and build beatiful landing page for my application

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import Header from "@/components/general/Header";
+import Faq from "@/components/landingPage/Faq";
+import FeatureHighlights from "@/components/landingPage/Features";
+import Footer from "@/components/landingPage/Footer";
+import HeroSection from "@/components/landingPage/HeroSection";
+import HowItWorks from "@/components/landingPage/HowItWorks";
+import Testinomials from "@/components/landingPage/Testinomials";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      hello
+      <HeroSection />
+      <FeatureHighlights />
+      <HowItWorks />
+      <Testinomials />
+      <Faq />
+      <Footer />
     </div>
   );
 }
